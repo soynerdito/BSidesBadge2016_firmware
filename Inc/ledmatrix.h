bd1f686383aabee744c9b8fa2d0aae34247b63e8
Soynerdito <soynerdito@gmail.com>
2016-04-14 22:02:53 -0400

    Adding matrix library to repo

---
+++ Inc/ledmatrix.h
@@ -0,0 +1,41 @@
+/*
+	Control led matrix for BSides badge
+*/
+#ifndef _LED_MATRIX
+#define _LED_MATRIX
+
+#include "stm32f0xx_hal.h"
+#include "spi_helper.h"
+
+
+/*
+ * Function pointers for structure
+*/
+typedef uint16_t (*type_spi_write_read)( uint8_t byte );
+typedef void (*type_clear)( void );
+typedef void (*type_write)(uint16_t Row, uint16_t Column );
+typedef uint32_t (*type_spi_read)(uint8_t readSize);
+typedef void (*type_initialize)( struct _spiControl* SPI_Control, GPIO_TypeDef * GPIOx, uint16_t GPIO_Pin );
+typedef void (*type_clear)( void );
+
+/*
+ *	Actual functions that read/write to the SPI
+*/
+void _latch( void );
+void _write(uint16_t Row, uint16_t Column );
+void _clear( void);
+void _initialize(struct _spiControl* SPI_Control, GPIO_TypeDef * GPIOx, uint16_t GPIO_Pin );
+
+/*
+ * Helper structure to call SPI in object like fashion
+ */
+struct _ledMatrix {
+	SPI_HandleTypeDef *SPI_Handle;
+	type_initialize initialize;
+	type_write write;	
+	type_clear clear;	
+}  ;
+
+extern struct _ledMatrix ledMatrix;
+
+#endif
---
+++ Src/ledmatrix.c
@@ -0,0 +1,42 @@
+
+#include "ledmatrix.h"
+
+
+//Actual structure that handles SPI communication
+struct _ledMatrix ledMatrix = { 0, _initialize, _write, _clear };
+
+/*
+	Hardware connection information
+*/
+GPIO_TypeDef* m_GPIOx_CS;
+uint16_t m_CS_Pin;
+struct _spiControl* m_SPI_Control;
+
+#define selectCS() \
+    CS_SET(m_GPIOx_CS, m_CS_Pin )
+
+#define resetCS() \
+    CS_RESET(m_GPIOx_CS, m_CS_Pin )
+
+void _write(uint16_t Row, uint16_t Column )
+{
+	m_SPI_Control->write( Row );
+	m_SPI_Control->write( Column );	
+	selectCS();
+	resetCS();
+	//selectCS();
+	//deselectCS();
+}
+
+void _clear( void)
+{
+	_write( 0x00, 0x00 );	
+}
+
+void _initialize(struct _spiControl* SPI_Control, GPIO_TypeDef * GPIOx, uint16_t GPIO_Pin )
+{
+	m_SPI_Control = SPI_Control;
+	m_GPIOx_CS = GPIOx;
+	m_CS_Pin = GPIO_Pin;
+	resetCS();
+}	
